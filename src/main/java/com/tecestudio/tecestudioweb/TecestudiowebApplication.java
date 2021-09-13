9a492a7c9c2ab05462a48795c6857965c586e7f8
Sergio Hernando Gutierrez homez <rashomez@gmail.com>
2021-09-13 12:17:45 -0500

    inicio

---
+++ src/main/java/com/tecestudio/tecestudioweb/TecestudiowebApplication.java
@@ -0,0 +1,13 @@
+package com.tecestudio.tecestudioweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecestudiowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecestudiowebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecestudio/tecestudioweb/TecestudiowebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecestudio.tecestudioweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecestudiowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
